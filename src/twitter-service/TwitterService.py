fe6ab7ad2e6f7a891f02ab3739933b88df164aa7
Gustavo Moser <gustavovbmoser@gmail.com>
2021-10-15 17:50:00 -0300

    some adjusts in twitter service

--- src/twitter-service/TwitterService.py
+++ src/twitter-service/TwitterService.py
@@ -4,15 +4,15 @@ from os import path, environ
 
 class TwitterService:
   def extract_tweets(self, query):
-    load_dotenv(dotenv_path=path.join(path.dirname(__file__), '..', '..'))
+    load_dotenv(dotenv_path=path.join(path.dirname(__file__), '..', '..', '.env'))
     
     bearer = environ.get('BEARER_TOKEN')
     
-    client = tweepy.Client(bearer_token=bearer)
+    client = tweepy.Client(bearer_token=bearer, wait_on_rate_limit=True)
     
     query = f'{query} -is:retweet'
     
-    result = client.search_all_tweets(query=query, tweet_fields=[])
+    result = client.search_recent_tweets(query=query, max_results=100) # max_results=100 tweet_fields=[]
     
     for tweet in result:
       self.persist(tweet)
@@ -21,3 +21,6 @@ class TwitterService:
   def persist(self, tweet):
     # TODO implement mongo interaction
     print(tweet)
+
+# service = TwitterService()
+# service.extract_tweets(query="covid masks")
